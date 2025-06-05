de6405f0f58667a278d507792f65596618f07e06
ARNAB DAS <raj713335@gmail.com>
2025-06-06 00:58:53 +0530

    :wrench: Adding Abstraction

--- 01 OOPs/05 abstraction.py
+++ 01 OOPs/05 abstraction.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 # Abstract class for Payment
 class Payment(ABC):
     def __init__(self, amount):
@@ -9,14 +10,17 @@ class Payment(ABC):
     def pay(self):
         ...
 
+
 # Implementing Payment Methods
 class CreditCardPayment(Payment):
     def pay(self):
         print(f"Paid {self.amount} using credit card")
 
+
 class PayPalPayment(Payment):
     def pay(self):
         print(f"Paid {self.amount} using PayPal")
+
 
 if __name__ == "__main__":
     payment_1 = CreditCardPayment(100)
