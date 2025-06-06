bf67f1dd2aefa94f8a3d15ffde7a8aeb24a73d8b
ARNAB DAS <raj713335@gmail.com>
2025-06-06 20:53:17 +0530

    :wrench: Adding Single Responsibility Principle

---
+++ 02 SOLID Design Principles/01 Single Responsibility Principle.py
@@ -0,0 +1,21 @@
+class UserManager:
+    def authenticate_user(self, username, password):
+        # Authentication Logic
+
+    def update_user_profile(self, user_id, new_profile_data):
+        # Profile update logic
+
+    def send_email_notifications(self, user_email, message):
+        # Email sending logic
+
+class UserAuthentication:
+    def authenticate_user(self, username, password):
+        # Authentication Logic
+
+class UserProfileManager:
+    def update_user_profile(self, user_id, new_profile_data):
+        # Profile update logic
+
+class EmailNotifier:
+    def send_email_notifications(self, user_email, message):
+        # Email sending logic
